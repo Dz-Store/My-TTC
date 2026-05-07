3fa5d7bece57c04e21e751617bbbe6d8a5cd0dc7
Dz-Store <benaakrabahcene@gmail.com>
2026-05-07 22:31:41 +0100

    تحديث إعدادات المتجر - 07/05/2026 22:31:41

--- config.js
+++ config.js
@@ -1,80 +1,31 @@
 const STORE_CONFIG = {
   "PRODUCTS": {
-    "4": {
-      "name": "Nike Baggy Jogger",
-      "price": 5000,
-      "image": "https://raw.githubusercontent.com/Dz-Store/WER/main/images/product-4-1763217318680-modal1.jpeg",
-      "description": " Nike Baggy Jogger  نوعية رفيعة بلونين مختلفين",
+    "19": {
+      "name": "مودال 1",
+      "price": 8400,
+      "image": "https://raw.githubusercontent.com/Dz-Store/My-TTC/main/images/product-19-1763505376624-1000056031.jpg",
+      "description": "dfgh",
       "availableSizes": [
-        "S",
         "M",
-        "L",
-        "XL"
+        "S2"
       ],
       "availableColors": [
         "أسود",
         "رمادي"
-      ],
-      "ageRanges": [],
-      "colorImages": {
-        "أسود": {
-          "images": [
-            "https://raw.githubusercontent.com/Dz-Store/WER/main/images/product-4-أسود-1763314062749-modal1.jpeg"
-          ],
-          "ageRanges": [
-            {
-              "ageFrom": "A",
-              "ageTo": "A",
-              "size": "S"
-            },
-            {
-              "ageFrom": "A",
-              "ageTo": "A",
-              "size": "M"
-            }
-          ]
-        },
-        "رمادي": {
-          "images": [
-            "https://raw.githubusercontent.com/Dz-Store/WER/main/images/product-4-رمادي-1763314176653-modal2.jpeg"
-          ],
-          "ageRanges": [
-            {
-              "ageFrom": "A",
-              "ageTo": "A",
-              "size": "S"
-            },
-            {
-              "ageFrom": "A",
-              "ageTo": "A",
-              "size": "M"
-            },
-            {
-              "ageFrom": "A",
-              "ageTo": "A",
-              "size": "L"
-            },
-            {
-              "ageFrom": "A",
-              "ageTo": "A",
-              "size": "XL"
-            }
-          ]
-        }
-      }
+      ]
     },
-    "5": {
-      "name": "مودال 2",
-      "price": 5600,
-      "image": "https://raw.githubusercontent.com/Dz-Store/My-TTC/main/images/product-5-1763549756272-modle6.jpg",
+    "20": {
+      "name": "sdegg",
+      "price": 75555,
+      "image": "https://raw.githubusercontent.com/Dz-Store/My-TTC/main/images/product-20-1763507188438-1000056027.jpg",
       "description": "",
       "availableSizes": [
-        "S",
         "S1",
-        "S2"
+        "XXL"
       ],
       "availableColors": [
-        "أحمر"
+        "كما في الصورة",
+        "أسود"
       ],
       "ageRanges": [],
       "colorImages": {}
@@ -310,182 +261,71 @@ const STORE_CONFIG = {
       "desk": 550
     },
     "58 - المنيعة": {
-      "home": 100,
+      "home": 1000,
       "desk": 400
     }
   },
   "DISCOUNTS": {
-    "minQuantityForDiscount": 1,
-    "discountPerItem": 800,
-    "discountScope": "selected",
+    "minQuantityForDiscount": 2,
+    "discountPerItem": 400,
     "discountProducts": [
-      5
+      19
     ],
+    "discountScope": "selected",
     "promoDiscountProducts": [
-      4
-    ],
-    "enableQuantityDiscount": false,
-    "enablePromotionalDiscount": true,
-    "promotionalDiscountPercent": 20,
-    "promotionalDiscountedPrice": null,
-    "promoDiscountScope": "selected",
-    "promoDiscountColors": [
-      1,
-      2
+      20
     ]
   },
   "STORE_INFO": {
-    "name": "SANIX-DZ",
-    "storeIcon": "👑",
-    "storeTitle": "Nike Baggy  Jogger",
+    "name": "إسم المتجر",
     "tagline": "متجر أفخم الملابس",
     "phoneNumbers": [
       "0671466489",
-      "0551102155"
+      "0792946548"
     ],
-    "facebookUrl": "https://www.facebook.com/share/r/1AUYY5favq/",
-    "instagramUrl": "https://www.instagram.com/p/DJCtlffoxj9/?igsh=MWoyaGFkYXRicmJrMQ==",
-    "messengerUrl": "https://www.instagram.com/p/DJCtlffoxj9/?igsh=MWoyaGFkYXRicmJrMQ==",
-    "viberNumber": "https://www.instagram.com/p/DJCtlffoxj9/?igsh=MWoyaGFkYXRicmJrMQ==",
-    "logo": "https://raw.githubusercontent.com/Dz-Store/My-TTC/main/images/product-store-logo-1764192107190.jpg",
-    "storeEmail": "benaakrabahcene@gmail.com",
-    "storeDescription": "متجر Kids-Fav يقدم أحدث وأفخم موديلات ملابس الأطفال بتصاميم عصرية وأنيقة تناسب جميع المناسبات",
-    "whatsappUrl": "https://wa.me/213671466489",
-    "whatsappNumber": "213671466489"
+    "storeIcon": "👗🛍️ الشعار",
+    "storeTitle": "وصف لعوان البوتيك",
+    "whatsappNumber": "",
+    "whatsappUrl": "",
+    "logo": "https://raw.githubusercontent.com/Dz-Store/My-TTC/main/images/product-store-logo-1764145388054.jpg"
   },
   "GOOGLE_SHEETS": {
-    "enabled": true,
-    "scriptUrl": "https://script.google.com/macros/s/AKfycbzw7NSduBIcVc2ugUKF5EtpmAej7bo7bDFKj0K-aHeXaYmqWmoUYtvOPGX6TjRKKN78/exec",
-    "ordersSheetId": "1U3ak4PZfAZR7oPUquLUETsv-ZhzrXR9v5XYSdTUeOxw",
-    "productsSheetId": "1U3ak4PZfAZR7oPUquLUETsv-ZhzrXR9v5XYSdTUeOxw",
-    "url": "https://docs.google.com/spreadsheets/d/1U3ak4PZfAZR7oPUquLUETsv-ZhzrXR9v5XYSdTUeOxw/edit?usp=drivesdk",
-    "autoUpdate": true,
-    "description": "جدول الطلبيات"
+    "url": "https://docs.google.com/spreadsheets/d/1U3ak4PZfAZR7oPUquLUETsv-ZhzrXR9v5XYSdTUeOxw/edit?gid=1320228317#gid=1320228317",
+    "autoUpdate": true
   },
   "AVAILABLE_COLORS": [
+    "كما في الصورة",
     "أبيض",
     "أسود",
-    "أحمر",
+    "رمادي",
     "أزرق",
+    "أحمر",
     "أخضر",
-    "وردي",
-    "رمادي"
+    "زهري",
+    "بنفسجي"
   ],
   "AVAILABLE_SIZES": [
-    "S",
     "S1",
     "S2",
-    "S3",
     "M",
-    "M1",
-    "M2",
-    "M3",
     "L",
     "XL",
-    "XXL",
-    "XXXL",
-    "38",
-    "39",
-    "40",
-    "41",
-    "42",
-    "43",
-    "44",
-    "45",
-    "45",
-    "46"
+    "XXL"
   ],
   "SIZE_GUIDE": {
-    "38": {
-      "heightFrom": 150,
-      "heightTo": 160,
-      "weightFrom": 50,
-      "weightTo": 70,
-      "enabled": false
-    },
-    "39": {
-      "heightFrom": 152,
-      "heightTo": 162,
-      "weightFrom": 53,
-      "weightTo": 74,
-      "enabled": false
-    },
-    "40": {
-      "heightFrom": 154,
-      "heightTo": 164,
-      "weightFrom": 56,
-      "weightTo": 78,
-      "enabled": false
-    },
-    "41": {
-      "heightFrom": 156,
-      "heightTo": 166,
-      "weightFrom": 59,
-      "weightTo": 82,
-      "enabled": false
-    },
-    "42": {
-      "heightFrom": 158,
-      "heightTo": 168,
-      "weightFrom": 62,
-      "weightTo": 86,
-      "enabled": false
-    },
-    "43": {
-      "heightFrom": 160,
-      "heightTo": 170,
-      "weightFrom": 65,
-      "weightTo": 90,
-      "enabled": false
-    },
-    "44": {
-      "heightFrom": 162,
-      "heightTo": 172,
-      "weightFrom": 68,
-      "weightTo": 94,
-      "enabled": false
-    },
-    "45": {
-      "heightFrom": 164,
-      "heightTo": 174,
-      "weightFrom": 71,
-      "weightTo": 98,
-      "enabled": false
-    },
-    "46": {
-      "heightFrom": 166,
-      "heightTo": 176,
-      "weightFrom": 74,
-      "weightTo": 102,
-      "enabled": false
-    },
-    "S": {
-      "heightFrom": 160,
-      "heightTo": 175,
-      "weightFrom": 50,
-      "weightTo": 65,
-      "enabled": true
-    },
     "S1": {
       "heightFrom": 160,
       "heightTo": 180,
       "weightFrom": 50,
       "weightTo": 80,
-      "enabled": false
+      "enabled": true
     },
     "S2": {
       "heightFrom": 160,
       "heightTo": 180,
       "weightFrom": 50,
       "weightTo": 80,
-      "enabled": false
-    },
-    "S3": {
-      "heightFrom": 160,
-      "heightTo": 180,
-      "weightFrom": 50,
-      "weightTo": 80,
-      "enabled": false
+      "enabled": true
     },
     "M": {
       "heightFrom": 170,
@@ -493,27 +333,6 @@ const STORE_CONFIG = {
       "weightFrom": 65,
       "weightTo": 80,
       "enabled": true
-    },
-    "M1": {
-      "heightFrom": 160,
-      "heightTo": 180,
-      "weightFrom": 50,
-      "weightTo": 80,
-      "enabled": false
-    },
-    "M2": {
-      "heightFrom": 160,
-      "heightTo": 180,
-      "weightFrom": 50,
-      "weightTo": 80,
-      "enabled": false
-    },
-    "M3": {
-      "heightFrom": 160,
-      "heightTo": 180,
-      "weightFrom": 50,
-      "weightTo": 80,
-      "enabled": false
     },
     "L": {
       "heightFrom": 180,
@@ -534,99 +353,16 @@ const STORE_CONFIG = {
       "heightTo": 205,
       "weightFrom": 95,
       "weightTo": 120,
-      "enabled": false
-    },
-    "XXXL": {
-      "heightFrom": 195,
-      "heightTo": 210,
-      "weightFrom": 105,
-      "weightTo": 140,
-      "enabled": false
+      "enabled": true
     }
   },
-  "COLORS": {
-    "1": {
-      "name": "الاحمر",
-      "price": 3000,
-      "images": [
-        "https://raw.githubusercontent.com/Dz-Store/WER/main/images/color-1-1762969529778-1000043662.jpg"
-      ],
-      "description": "مودال جيد بمواصفات أوربية",
-      "availableSizes": [
-        "S",
-        "S1"
-      ],
-      "ageRanges": [
-        {
-          "ageFrom": 2,
-          "ageTo": 5,
-          "size": "S"
-        },
-        {
-          "ageFrom": 6,
-          "ageTo": 8,
-          "size": "S1"
-        }
-      ]
-    },
-    "2": {
-      "name": "ازرق",
-      "price": 3000,
-      "images": [
-        "https://raw.githubusercontent.com/Dz-Store/WER/main/images/color-2-1762969972266-1000041608.jpg"
-      ],
-      "description": "مودال بمواصفات اوربية",
-      "availableSizes": [
-        "S2",
-        "S3"
-      ],
-      "ageRanges": [
-        {
-          "ageFrom": 6,
-          "ageTo": 8,
-          "size": "S2"
-        },
-        {
-          "ageFrom": 9,
-          "ageTo": 11,
-          "size": "S3"
-        }
-      ]
-    }
-  },
-  "ADMIN_SETTINGS": {
-    "password": "1234",
-    "autoSync": true,
-    "backupEnabled": true,
-    "notifications": true,
-    "lastUpdated": "2024-11-12T10:00:00.000Z"
-  },
-  "COLOR_IMAGES_MANAGEMENT": {
-    "enabled": true,
-    "maxImagesPerColor": 10,
-    "allowedFormats": [
-      "jpg",
-      "jpeg",
-      "png",
-      "webp"
-    ]
-  },
-  "AGE_RANGES_MANAGEMENT": {
-    "enabled": true,
-    "minAge": 2,
-    "maxAge": 15
-  },
-  "GITHUB_INTEGRATION": {
-    "enabled": true,
-    "defaultRepo": "Dz-Store/WER",
-    "defaultBranch": "main",
-    "defaultFolder": "images"
-  },
-  "SYSTEM_SETTINGS": {
-    "autoBackup": true,
-    "backupInterval": 24,
-    "maxBackups": 30,
-    "enableNotifications": true,
-    "enableAnalytics": true
+  "AGE_SIZES": {
+    "6": "S1",
+    "7": "S2",
+    "8": "M",
+    "9": "L",
+    "10": "XL",
+    "11": "XXL",
+    "12": "S4"
   }
 };
